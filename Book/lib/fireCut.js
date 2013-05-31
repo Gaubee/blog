d57c6cde18b329a508e393309150e652468e3116
gaubee <gaubeebangeel@gmail.com>
2013-05-31 18:09:58 +0800

    基于nodejs的文章切割工具，并上传切割出的章节
    Signed-off-by: gaubee <gaubeebangeel@gmail.com>

---
+++ Book/lib/fireCut.js
@@ -0,0 +1,40 @@
+var fs = require("fs");
+var basePath = "../Masterpiece";
+var bookPath = "/Pride and Prejudice/";
+var fileName = "Pride and Prejudice.md";
+
+function writeFile(fs,filePath,fileName,data,callback){
+	fs.open(filePath+fileName,"w",0644,function(err,fd){//open or create
+		if(err) throw err;
+		if ((typeof data).toString()==="object") {
+			data = JSON.stringify(data);
+		}else{
+			data = data.toString();
+		}
+		fs.write(fd,data,0,"utf8",function(err){
+			if(err) throw err;
+			callback&&callback(fs,filePath,fileName,data);
+			fs.closeSync(fd);
+		})
+	})
+}
+
+fs.readFile(basePath+bookPath+fileName,
+	{
+		encoding:"utf8",
+		flag :"r"
+	},
+	function(err,fileData){
+		if(err){
+			throw err;
+		}
+		var chapters = fileData.toString().split("### Chapter");
+		var perChapter;
+		var Length = chapters.length;
+		// console.log(Chapters[1]);
+		for(var i =1;i<Length;i+=1){
+			perChapter = chapters[i];
+			console.log("Chapter"+perChapter.split("\n")[0])
+			writeFile(fs,basePath+bookPath+"Chapter/",("Chapter"+perChapter.split("\n")[0])+".md","### Chapter"+perChapter);
+		}
+	});
